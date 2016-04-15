2e12401b781675425ab127b78ea35919d23ed760
meshnesh <tonnie.nesh@gmail.com>
2016-04-15 08:19:14 +0300

    added the contact form and styling it in footer

--- js/app1.js
+++ js/app1.js
@@ -1,7 +1,51 @@
 //$(document).foundation();
-//$(".off-canvas-submenu").hide();
-//$(".off-canvas-submenu-call").click(function() {
-//	var icon = $(this).parent().next(".off-canvas-submenu").is(':visible') ? '+' : '-';
-//	$(this).parent().next(".off-canvas-submenu").slideToggle('fast');
-//	$(this).find("span").text(icon);
+//$('textarea').blur(function() {
+//  $('#hire textarea').each(function() {
+//    $this = $(this);
+//    if (this.value != '') {
+//      $this.addClass('focused');
+//      $('textarea + label + span').css({
+//        'opacity': 1
+//      });
+//    } else {
+//      $this.removeClass('focused');
+//      $('textarea + label + span').css({
+//        'opacity': 0
+//      });
+//    }
+//  });
+//});
+//
+//$('#hire .field:first-child input').blur(function() {
+//  $('#hire .field:first-child input').each(function() {
+//    $this = $(this);
+//    if (this.value != '') {
+//      $this.addClass('focused');
+//      $('.field:first-child input + label + span').css({
+//        'opacity': 1
+//      });
+//    } else {
+//      $this.removeClass('focused');
+//      $('.field:first-child input + label + span').css({
+//        'opacity': 0
+//      });
+//    }
+//  });
+//});
+//
+//$('#hire .field:nth-child(2) input').blur(function() {
+//  $('#hire .field:nth-child(2) input').each(function() {
+//    $this = $(this);
+//    if (this.value != '') {
+//      $this.addClass('focused');
+//      $('.field:nth-child(2) input + label + span').css({
+//        'opacity': 1
+//      });
+//    } else {
+//      $this.removeClass('focused');
+//      $('.field:nth-child(2) input + label + span').css({
+//        'opacity': 0
+//      });
+//    }
+//  });
 //});
